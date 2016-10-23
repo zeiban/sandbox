72189a9b406d629dfdf6108c24c2b9594d694836
Zeiban <me@zeiban.com>
2016-10-23 17:52:01 -0400

    changes

--- DX11Test01/DX11Test01/Input.h
+++ DX11Test01/DX11Test01/Input.h
@@ -3,6 +3,50 @@
 #include <vector>
 #include "InputDevice.h"
 
+enum InputDeviceType
+{
+	Mouse,
+	Keyboard
+};
+
+struct InputMap
+{
+
+};
+enum InputId
+{
+	A,
+	S,
+	D,
+	W,
+	S,
+	LEFT,
+	RIGHT,
+	UP,
+	DOWN,
+	Mouse1,
+	Mouse2,
+	MouseX,
+	MouseY
+};
+
+enum InputState
+{
+	Pressed,
+	Released,
+	Changed
+};
+
+struct InputEvent
+{
+
+	InputDeviceType type;
+	InputState state;
+	InputId id;
+	float value;
+	InputEvent() {}
+};
+
 class Input
 {
 private:
--- DX11Test01/DX11Test01/InputEvent.h
+++ DX11Test01/DX11Test01/InputEvent.h
@@ -1,5 +1,4 @@
 #pragma once
-
 struct InputEvent
 {
 	enum Type
